12956932daf2e5f8eee71cfb51b9977d533269a8
Marcos Paulo Arruda Frare <marcosf@4xdigital.ai>
2025-06-30 18:55:39 -0300

    created generate token jwt sample

---
+++ 07-GenerateTokenJwt/GenerateTokenJwt/GenerateTokenJwt.Sample/Claims.cs
@@ -0,0 +1,8 @@
+﻿namespace GenerateTokenJwt.Sample;
+
+public static class Claims
+{
+    public const string SellerId = "sellerId";
+    public const string Email = "email";
+    public const string Exp = "exp";
+}
---
+++ 07-GenerateTokenJwt/GenerateTokenJwt/GenerateTokenJwt.Sample/Result.cs
@@ -0,0 +1,17 @@
+﻿namespace GenerateTokenJwt.Sample;
+
+public class Result
+{
+    public bool IsSuccess { get; }
+    public string Error { get; }
+
+    private Result(bool isSuccess, string? error = null)
+    {
+        IsSuccess = isSuccess;
+        Error = error ?? string.Empty;
+    }
+
+    public static Result Success() => new(isSuccess: true);
+
+    public static Result Failure(string error) => new(isSuccess: false, error);
+}
